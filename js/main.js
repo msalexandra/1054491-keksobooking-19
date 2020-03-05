eeaa2625b68b696885c46443ac1045aabe14318a
msalexandra <mr.darthv@ya.ru>
2020-03-05 22:26:58 +0300

    Вопросы по четвертому и третьему пунктам

--- js/main.js
+++ js/main.js
@@ -3,92 +3,90 @@
 var сreateAdvertisement = function (title, price, rooms, guests, description, coordinateX, coordinateY, typeFix, wigth, checkinFix, checkoutFix, higth) {
 	var arr = new Array();
 	
-
- 	for (var i = 1; i < 9; i++) {
+	for (var i = 1; i < 9; i++) {
  		var numFeatures = randomInteger (1, 6);
  		var wigthX = randomInteger (1, wigth);
  		var numPhotos = randomInteger (1, 3);
  		var higthY = randomInteger (130, 630);
 
-	switch (numFeatures) {
-		case 1:
-		 var = features { "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"};
-		break;
+		switch (numFeatures) {
+			case 1:
+			var = features {"wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"};
+			break;
+			
+			case 2:
+		 	var = features {"wifi", "dishwasher", "parking", "washer", "elevator"};
+			break;
 
-		case 2:
-		 var = features { "wifi", "dishwasher", "parking", "washer", "elevator"};
-		break;
+			case 3:
+			var = features {"wifi", "dishwasher", "parking", "washer"};
+			break;
 
-		case 3:
-		 var = features { "wifi", "dishwasher", "parking", "washer"};
-		break;
+			case 4:
+		 	var = features {"wifi", "dishwasher", "parking"};
+			break;
 
-		case 4:
-		 var = features { "wifi", "dishwasher", "parking"};
-		break;
+			case 5:
+			var = features {"wifi", "dishwasher"};
+			break;
+	
+			case 6:
+			var = features {"wifi"};
+			break;
+		}
 
-		case 5:
-		 var = features { "wifi", "dishwasher"};
-		break;
+		switch (numPhotos){
+			case 1:
+			var = photos { "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"};
+			break;
 
-		case 6:
-		 var = features { "wifi"};
-		break;
-	}
+			case 2:
+		 	var = photos { "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg"};
+			break;
 
-	switch (numPhotos){
-		case 1:
-		 var = photos { "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"};
-		break;
+			case 3:
+		 	var = photos { "http://o0.github.io/assets/images/tokyo/hotel1.jpg"};
+			break;
+		}
 
-		case 2:
-		 var = photos { "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg"};
-		break;
-
-		case 3:
-		 var = photos { "http://o0.github.io/assets/images/tokyo/hotel1.jpg"};
-		break;
-	}
-
-
-	if (typeFix == 1) {
-		var type  = "palace";
-	}
-	else {
-		if (typeFix == 2) {
-			var type  = "flat";
+		if (typeFix == 1) {
+			var type  = "palace";
 		}
 		else {
-			if (typeFix == 3) {
-				var type  = "house";
+			if (typeFix == 2) {
+				var type  = "flat";
 			}
 			else {
-				if (typeFix == 4) {
-					var type  = "bungalo";
+				if (typeFix == 3) {
+					var type  = "house";
 				}
 				else {
-					var type  = "NaN";
+					if (typeFix == 4) {
+						var type  = "bungalo";
+					}
+					else {
+						var type  = "NaN";
+					}
 				}
 			}
 		}
-	}
 
-	if (checkinFix == 1) {
-		var checkin = "12:00";
-	}
-	else {
-		if (checkinFix == 2) {
-			var checkin = "13:00 ";
+		if (checkinFix == 1) {
+			var checkin = "12:00";
 		}
-		else {
-			if (checkinFix == 3) {
-				var checkin = "14:00";
+			else {
+			if (checkinFix == 2) {
+				var checkin = "13:00 ";
 			}
 			else {
-				var checkin = "NaN";
-			}	
+				if (checkinFix == 3) {
+					var checkin = "14:00";
+				}
+				else {
+					var checkin = "NaN";
+				}	
+			}
 		}
-	}
 
 	if (checkoutFix == 1) {
 		var checkout = "12:00";
@@ -108,8 +106,7 @@ var сreateAdvertisement = function (title, price, rooms, guests, description, c
 
 	}
 
-
- 		var adressImg = "img/avatars/user0" + i + ".png";
+		var adressImg = "img/avatars/user0" + i + ".png";
  		var address = coordinateX + ", " + coordinateY;
  		var advertisement = {
         "author": {
@@ -133,10 +130,7 @@ var сreateAdvertisement = function (title, price, rooms, guests, description, c
     "y": higthY
     }
 };
- 		arr[i] = advertisement;
-
- 	
-	};
+ 		arr[i] = advertisement;};
 };
 
 var elementLocation = function (wigthX, higthY, imgUrl){
@@ -148,14 +142,8 @@ var elementLocation = function (wigthX, higthY, imgUrl){
 	pins.style = 'left:' + wigthX + width_pin + 'px top:' + higthY + height_pin + 'px';
 	pins.src = imgUrl;
 	pins.alt = 'Метки объявлений';
-
 }
 
 	//новая функция с пунктом 4
 
-/* <template id="pin">
-    <button type="button" class="map__pin" style="left: 200px; top: 400px;"><img src="img/avatars/user07.png" width="40" height="40" draggable="false" alt="Метка объявления"></button>
-  </template>*/
-
-  //pin.getAttribute(width);
 
